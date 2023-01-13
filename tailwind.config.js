fb6a3662963f40aeed9837c72bb3133c440c1828
corentinthibaud <corentin.thibaud@isen.yncrea.fr>
2023-01-13 23:08:46 +0100

    new: adding colors and removing the default CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "white": "#faf4f4",
+      "orange": "#ef962e",
+      "brown": "#9c5619",
+      "brown-light": "#5b4012",
+      "floral-white": "#fffaf0",
+    },
     extend: {},
   },
   plugins: [],
