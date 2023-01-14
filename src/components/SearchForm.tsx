da658850056f96ae98719d9f7573c85c695c32be
corentinthibaud <corentin.thibaud@isen.yncrea.fr>
2023-01-14 03:21:19 +0100

    new: Datepicker done

---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,65 @@
+import {Raleway} from "@next/font/google";
+import Flatpickr from "react-flatpickr";
+
+import "flatpickr/dist/themes/airbnb.css";
+import "flatpickr/dist/l10n/fr.js"
+import flatpickr from "flatpickr";
+import CustomLocale = flatpickr.CustomLocale;
+
+const inter = Raleway({subsets: ['latin']})
+
+export default function SearchForm() {
+	flatpickr.localize(flatpickr.l10ns.fr as CustomLocale)
+	const date = new Date()
+	let min_date, end_date = new Date()
+	return (
+		<>
+			<div className={"d-flex absolute flex-column text-center max-w-440"}>
+				<div className={"d-flex flex-row border-bottom"}>
+					<div className={"d-flex flex-column p-4 border-end"} id={"start_date_form_input"}>
+						<b>Date d'arrivée</b>
+						<Flatpickr
+						    onChange={(dates, currentDateString, self, data) => {
+								min_date = dates[dates.length - 1]
+							}}
+						    id={"start_date_form"}
+						    options={{minDate: date, defaultDate: date, dateFormat: "d/m/Y"}}
+						    render={
+							   ({defaultValue, value, ...props}, ref) => {
+								   return <CustomInput defaultValue={defaultValue} inputRef={ref} />
+							   }
+						    }
+						/>
+					</div>
+					<div className={"d-flex flex-column p-4"} id={"end_date_form_input"}>
+						<b>Date de départ</b>
+						<Flatpickr
+							onChange={(dates, currentDateString, self, data) => {
+								end_date = dates[dates.length - 1]
+							}}
+							id={"end_date"}
+							options={{minDate: min_date, defaultDate: date, dateFormat: "d/m/Y"}}
+							render={
+								({defaultValue, value, ...props}, ref) => {
+									return <CustomInput defaultValue={defaultValue} inputRef={ref} />
+								}
+							}
+						/>
+					</div>
+				</div>
+				<div className={"d-flex flex-column "}>
+					<b>Nombre de personnes</b>
+					<input type={"number"} defaultValue={"2"} className={"text-center border-none"}/>
+				</div>
+				<button type={"button"} className={"btn btn-primary mt-2"}>Réserver</button>
+			</div>
+		</>
+	)
+}
+
+function CustomInput({ value, defaultValue, inputRef, ...props}) {
+	return (
+		<input {...props} value={value} defaultValue={defaultValue} ref={inputRef} type={"date"} className={"border-none" +
+			" text-center"}/>
+	)
+}
